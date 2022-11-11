e3029bddb371bbdfaadfc564e3142ff0c888ca9d
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-11-11 12:04:34 -0300

    update styles

--- script/script.js
+++ script/script.js
@@ -3,12 +3,16 @@ import Card from "./modules/skills.js";
 import listSkill from './modules/listSkill.js'
 import animaScroll from './modules/animaScroll.js'
 
+
+
 menuMobile()
 const scrollAnima = animaScroll('[data-anima]')
 scrollAnima()
 const skillList =listSkill()
 const cardSkill = new Card('.conteudo-skill','.skill-text p',skillList)
 cardSkill.init()
+
+
 
 
 
