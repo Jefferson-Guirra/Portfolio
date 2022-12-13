c1b2430d3a3636c4aaa8e306b9466700a83fb17e
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-12-12 21:16:40 -0300

    feature/active nav link with scroll

---
+++ script/modules/activeIconScrollMenu.js
@@ -0,0 +1,54 @@
+export default function activeIcon() {
+  function onScroll() {
+    activateMenuAtCurrentSection('presetation')
+    activateMenuAtCurrentSection('sobre')
+    activateMenuAtCurrentSection('projetos')
+    activateMenuAtCurrentSection('conhecimentos')
+    activateMenuAtCurrentSection('contact')
+  }
+  function activateMenuAtCurrentSection(id) {
+    const section = document.getElementById(id)
+
+    //linha alvo
+    const targetLine = scrollY + innerHeight / 2
+
+    //verificar se a seção passou da linha
+    //quais dados vou precisar
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    //o topo da seção chegou ou ultrapassou a linha alvo
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+
+
+    //verificar se a base esta abaixo da linha alvo
+    //quais dados vou precisar
+
+    //a seção termina onde
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    //O final da seção passou da linha alvo
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    
+
+    //limites da seçao
+    const sectionBoundaries =
+      sectionTopReachOrPassedTargetline && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    
+
+    const menuElement = document.querySelector(`.navLinks a[href*=${sectionId}]`)
+    
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+      menuElement.classList.add('active')
+    }
+  } 
+  window.onload=()=>{
+    onScroll()
+  }
+  window.addEventListener('scroll',onScroll)
+}
--- script/script.js
+++ script/script.js
@@ -1,16 +1,17 @@
-import menuMobile from "./modules/menuMobile.js";
 import Card from "./modules/skills.js";
 import listSkill from './modules/listSkill.js'
 import animaScroll from './modules/animaScroll.js'
 import slider from './modules/slider.js'
+import activeIcon from "./modules/activeIconScrollMenu.js";
 
 
-/*menuMobile()*/
+
 const scrollAnima = animaScroll('[data-anima]')
 scrollAnima()
 const skillList =listSkill()
 const cardSkill = new Card('.conteudo-skill','.skill-text p',skillList)
 cardSkill.init()
+activeIcon()
 slider('.cards','.spin')
 slider('.skill-card', '.spinCard')
 
