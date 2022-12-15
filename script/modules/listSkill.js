9b7a98067312e5f9a01d922b87718ffa4ac94e74
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-12-15 18:27:53 -0300

    feat: change text submit form  in the contact section

--- script/modules/listSkill.js
+++ script/modules/listSkill.js
@@ -12,7 +12,7 @@ export default function listSkill (){
     5: 'Next.js é um poderoso framework baseado em react que possui funcionalidades como renderização ao lado do servidor e geração de páginas estáticas para web.',
     6: 'Redux é uma biblioteca para armazenamento de estados de aplicações JavaScript.',
     7:'Firebase é uma plataforma usada para desenvolvimento de  apps, ela fornece diversos serviços, desde autenticação de usuários com firebase authentication ao armazenamento de dados.',
-    8:'Prismic é um CMS ou seja é um sistema de gerenciamento de conteúdo, quando usado  ações de criação, remoção, adição e modificação de um conteúdo tornam-se simples, sem a necssidade de conhecimento em programação.',
+    8:'Prismic é um CMS ou seja é um sistema de gerenciamento de conteúdo, quando usado  ações de criação, remoção, adição e modificação de um conteúdo tornam-se simples, sem a necessidade de conhecimento em programação.',
     9: 'Bootstrap é um framework desenvolvido para pontencializar o bom e velho Css.',
     10: 'Styled Components é uma biblioteca que utiliza o conceito de Css-in-JS, ou seja, que nos permite escrever códigos CSS dentro do JavaScript',
     11: 'Jest é um framework de testes, usado para prevenção de erros.',
