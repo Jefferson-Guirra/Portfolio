4bdaff90ee0b496ed77a85ab7a1d27b694ec42a8
jefferson guirra <jeffersonguirra8@gmail.com>
2023-01-30 19:23:56 -0300

    feat: add nodejs skill card

--- script/modules/listSkill.js
+++ script/modules/listSkill.js
@@ -7,15 +7,16 @@ export default function listSkill (){
     2: 'JavaScript é uma linguagem de programação interpretada e estruturada, de script de alto nível com tipagem dinâmica fraca e multiparadigma.',
 
     3: 'TypeScript é um superSet da linguagem JavaScript de tipagem estática, quando usado potencializa a detecção de erros durante o desenvolvimento e habilita diferentes funcionalidades não nativas.',
+    4: 'Nodejs é um ambiente baseado no motor v8 responsável por executar javascript no servidor.',
 
-    4: 'ReactJS é uma biblioteca JavaScript de código aberto usada para a criação de interfaces de usuário em páginas web.',
-    5: 'Next.js é um poderoso framework baseado em react que possui funcionalidades como renderização ao lado do servidor e geração de páginas estáticas para web.',
-    6: 'Redux é uma biblioteca para armazenamento de estados de aplicações JavaScript.',
-    7:'Firebase é uma plataforma usada para desenvolvimento de  apps, ela fornece diversos serviços, desde autenticação de usuários com firebase authentication ao armazenamento de dados.',
-    8:'Prismic é um CMS ou seja é um sistema de gerenciamento de conteúdo, quando usado  ações de criação, remoção, adição e modificação de um conteúdo tornam-se simples, sem a necessidade de conhecimento em programação.',
-    9: 'Bootstrap é um framework desenvolvido para pontencializar o bom e velho Css.',
-    10: 'Styled Components é uma biblioteca que utiliza o conceito de Css-in-JS, ou seja, que nos permite escrever códigos CSS dentro do JavaScript',
-    11: 'Jest é um framework de testes, usado para prevenção de erros.',
+    5: 'ReactJS é uma biblioteca JavaScript de código aberto usada para a criação de interfaces de usuário em páginas web.',
+    6: 'Next.js é um poderoso framework baseado em react que possui funcionalidades como renderização ao lado do servidor e geração de páginas estáticas para web.',
+    7: 'Redux é uma biblioteca para armazenamento de estados de aplicações JavaScript.',
+    8:'Firebase é uma plataforma usada para desenvolvimento de  apps, ela fornece diversos serviços, desde autenticação de usuários com firebase authentication ao armazenamento de dados.',
+    9:'Prismic é um CMS ou seja é um sistema de gerenciamento de conteúdo, quando usado  ações de criação, remoção, adição e modificação de um conteúdo tornam-se simples, sem a necessidade de conhecimento em programação.',
+    10: 'Bootstrap é um framework desenvolvido para pontencializar o bom e velho Css.',
+    11: 'Styled Components é uma biblioteca que utiliza o conceito de Css-in-JS, ou seja, que nos permite escrever códigos CSS dentro do JavaScript',
+    12: 'Jest é um framework de testes, usado para prevenção de erros.',
  
   }
   return listInfo
