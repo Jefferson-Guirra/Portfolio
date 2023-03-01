dfea1a311917e8f2739b040aafcf503c352bcfa1
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-01 16:33:55 -0300

    refactor: move all creation swiperSlider

---
+++ script/modules/createSwiperSlider.js
@@ -0,0 +1,44 @@
+const optionsSlider =  () => {
+    let optionsSlider = []
+    const optionsSlider1 = {
+    name:'.mySwiper',
+    loop:'true',
+    effect: 'flip',
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+    }
+    }
+
+    const optionsSlider2 ={
+    name:'.skillSwiper',
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+    },
+    breakpoints:{
+    500:{
+        slidesPerView:3,
+        spaceBetween:5
+    },
+    300:{
+        slidesPerView:2,
+        spaceBetween:15
+    }
+    }
+    }
+
+    optionsSlider.push(optionsSlider1)
+    optionsSlider.push(optionsSlider2)
+    return optionsSlider
+}
+const createSwiperSlider = () => {
+    const options =  optionsSlider()
+    options.forEach(item =>new Swiper(item.name,item))
+}
+
+
+export default createSwiperSlider
+  
--- script/script.js
+++ script/script.js
@@ -2,36 +2,9 @@ import Card from "./modules/skills.js";
 import listSkill from './modules/listSkill.js'
 import animaScroll from './modules/animaScroll.js'
 import activeIcon from "./modules/activeIconScrollMenu.js";
+import createSwiperSlider from "./modules/createSwiperSlider.js";
 
-
-const swiper = new Swiper('.mySwiper', {
-  loop:'true',
-  effect: 'flip',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  }
-})
-const skillsSwiper = new Swiper('.skillSwiper', {
-  slidesPerView: 3,
-  spaceBetween: 10,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  breakpoints:{
-    500:{
-      slidesPerView:3,
-      spaceBetween:5
-    },
-    300:{
-      slidesPerView:2,
-      spaceBetween:15
-    }
-  }
-  
-})
-
+createSwiperSlider()
 const scrollAnima = animaScroll('[data-anima]')
 scrollAnima()
 const skillList =listSkill()
