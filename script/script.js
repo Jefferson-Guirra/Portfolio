134d7878bdbe9069d461d6966567f20adf92d48d
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-01 15:49:33 -0300

    refactor: remove unused Slider function

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 import Card from "./modules/skills.js";
 import listSkill from './modules/listSkill.js'
 import animaScroll from './modules/animaScroll.js'
-import slider from './modules/slider.js'
 import activeIcon from "./modules/activeIconScrollMenu.js";
 
 
